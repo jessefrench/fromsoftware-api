dbe382ba6f12499b71fd46c338184be759246e81
Jesse French <frejes32@gmail.com>
2022-08-18 16:42:22 -0500

    first commit - working api

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const games = {
+    "demon's souls":{
+        'releaseYear': 2009,
+        'copiesSold': '135 thousand',
+        'metacriticScore': 89
+    },
+    'dark souls':{
+        'releaseYear': 2011,
+        'copiesSold': '2.3 million',
+        'metacriticScore': 89
+    },
+    'dark souls ii':{
+        'releaseYear': 2014,
+        'copiesSold': '2.5 million',
+        'metacriticScore': 91
+    },
+    'dark souls iii':{
+        'releaseYear': 2016,
+        'copiesSold': '3.2 million',
+        'metacriticScore': 89
+    },
+    'bloodborne':{
+        'releaseYear': 2015,
+        'copiesSold': '2 million',
+        'metacriticScore': 92
+    },
+    'sekiro':{
+        'releaseYear': 2019,
+        'copiesSold': '5 million',
+        'metacriticScore': 91
+    },
+    'elden ring':{
+        'releaseYear': 2022,
+        'copiesSold': '16.6 million',
+        'metacriticScore': 96
+    },
+    'git gud':{
+        'releaseYear': 'Git Gud',
+        'copiesSold': 'Git Gud',
+        'metacriticScore': 'Git Gud'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:gameTitle', (request, response) => {
+    const titles = request.params.gameTitle.toLowerCase()
+    if(games[titles]){
+        response.json(games[titles])
+    }else{
+        response.json(games['git gud'])
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is running on port ${PORT}.`)
+})
